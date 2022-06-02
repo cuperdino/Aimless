f9a048e0a8e222a252c7e74d7c9e82d0e88e9a82
Sabahudin Kodro <sabahudin.kodro@gmail.com>
2022-06-02 21:57:57 +0200

    Move models to seperate file

---
+++ Packages/Sources/ApiClient/Models.swift
@@ -0,0 +1,52 @@
+//
+//  Models.swift
+//  
+//
+//  Created by Sabahudin Kodro on 02/06/2022.
+//
+
+import Foundation
+
+public struct Todo: Codable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let completed: Bool
+}
+
+public struct User: Codable {
+    let id: Int
+    let name: String
+    let username: String
+    let email: String
+}
+
+// Custom decoding for post response
+public struct PostResponse<Model: Codable>: Codable {
+    let modelArray: [Model]
+
+    private struct DynamicCodingKeys: CodingKey {
+        var stringValue: String
+        init?(stringValue: String) {
+            self.stringValue = stringValue
+        }
+        var intValue: Int?
+        init?(intValue: Int) {
+            return nil
+        }
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: DynamicCodingKeys.self)
+        var tempArray = [Model]()
+        for key in container.allKeys {
+            guard key.stringValue != "id" else { continue }
+            let decodedObject = try container.decode(
+                Model.self,
+                forKey: DynamicCodingKeys(stringValue: key.stringValue)!
+            )
+            tempArray.append(decodedObject)
+        }
+        modelArray = tempArray
+    }
+}
