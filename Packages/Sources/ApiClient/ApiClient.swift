fceb4ed334794f089ab80707478450e1a60809f6
Sabahudin Kodro <sabahudin.kodro@gmail.com>
2022-06-02 19:14:25 +0200

    Add more generic api with errors

--- Packages/Sources/ApiClient/ApiClient.swift
+++ Packages/Sources/ApiClient/ApiClient.swift
@@ -1,32 +1,63 @@
 import Foundation
 
-final class ApiClient {
-    enum ApiError: Error {
-        case invalidUrl
+// Transport maps a URLRequest to Data.
+// Makes it easily extendable and testable.
+// Simple but powerful idea, taken from
+// Rob Napier: http://robnapier.net/a-mockery-of-protocols
+public protocol Transport {
+    func send(request: URLRequest) async throws -> Data
+}
+
+// Check for a couple of
+// standard errors
+enum ApiError: Error, Equatable {
+    case interalServerError
+    case notFound
+    case other(String)
+
+    init(statusCode: Int) {
+        if statusCode == 500 {
+            self = .interalServerError
+        } else if statusCode == 404 {
+            self = .notFound
+        } else {
+            let localizedString = HTTPURLResponse.localizedString(forStatusCode: statusCode)
+            self = .other(localizedString)
+        }
+    }
+}
+
+public extension URLResponse {
+    func validate() throws {
+        if let httpResponse = self as? HTTPURLResponse {
+            if !(200..<300).contains(httpResponse.statusCode) {
+                throw ApiError(statusCode: httpResponse.statusCode)
+            }
+        }
+    }
+}
+
+extension URLSession: Transport {
+    public func send(request: URLRequest) async throws -> Data {
+        let (data, response) = try await self.data(for: request)
+        try response.validate()
+        return data
+    }
+}
+
+class ApiClient {
+    let transport: Transport
+
+    init(transport: Transport = URLSession.shared) {
+        self.transport = transport
     }
 
-    let baseUrL = "https://jsonplaceholder.typicode.com"
-
-    func fetchTodos() async throws -> [Todo] {
-        guard let url = URL(string: baseUrL) else {
-            throw ApiError.invalidUrl
-        }
-        let postsUrl = url.appendingPathComponent("todos")
-        let (data, _) = try await URLSession.shared.data(from: postsUrl)
-
-        let posts = try JSONDecoder().decode([Todo].self, from: data)
-        return posts
+    func send<T: Codable>(request: URLRequest) async throws -> T {
+        let data = try await transport.send(request: request)
+        return try JSONDecoder().decode(T.self, from: data)
     }
+}
 
-    func fetchUsers() async throws -> [User] {
-        guard let url = URL(string: baseUrL) else {
-            throw ApiError.invalidUrl
-        }
-        let postsUrl = url.appendingPathComponent("users")
-        let (data, _) = try await URLSession.shared.data(from: postsUrl)
-
-        let users = try JSONDecoder().decode([User].self, from: data)
-        return users
     }
 }
 
