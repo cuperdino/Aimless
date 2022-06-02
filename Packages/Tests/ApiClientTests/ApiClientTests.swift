a3fd36955d62187738e514c1c6fb56e2e9a20cdd
Sabahudin Kodro <sabahudin.kodro@gmail.com>
2022-06-02 20:09:24 +0200

    Improve throwable async await test call site

--- Packages/Tests/ApiClientTests/ApiClientTests.swift
+++ Packages/Tests/ApiClientTests/ApiClientTests.swift
@@ -6,41 +6,64 @@ final class ApiClientTests: XCTestCase {
     let apiClient = ApiClient()
 
     func testNotFoundError() async throws {
-        let apiError = await getApiError(statusCode: 404)
-        XCTAssertEqual(ApiError.notFound, apiError)
-    }
-
-    func testNotInternalServerError() async throws {
-        let apiError = await getApiError(statusCode: 500)
-        XCTAssertEqual(ApiError.interalServerError, apiError)
-    }
-
-    func testOtherError() async throws {
-        let apiError = await getApiError(statusCode: 504)
-        switch apiError {
-        case .other: assert(true)
-        default: assert(false)
+        await XCTAssertThrowsError(try await testApiError(withStatusCode: 404)) { error in
+            guard case .notFound = error as! ApiError else {
+                return XCTFail("Expected .notFound, but got \(error)")
+            }
+            XCTAssert(true)
         }
     }
 
-    private func getApiError(statusCode: Int) async -> ApiError {
-        let request = URLRequest(url: URL(string: "testurl.com")!)
+    func testInternalServerError() async throws {
+        await XCTAssertThrowsError(try await testApiError(withStatusCode: 500)) { error in
+            guard case .interalServerError = error as! ApiError else {
+                return XCTFail("Expected .interalServerError, but got \(error)")
+            }
+            XCTAssert(true)
+        }
+    }
 
+    func testOtherError() async throws {
+        await XCTAssertThrowsError(try await testApiError(withStatusCode: 505)) { error in
+            guard case .other = error as! ApiError else {
+                return XCTFail("Expected .other, but got \(error)")
+            }
+            XCTAssert(true)
+        }
+    }
+
+    private func testApiError(withStatusCode statusCode: Int) async throws -> Data {
+        let request = URLRequest(url: URL(string: "testurl.com")!)
         let urlResponse = HTTPURLResponse(
             url: request.url!,
             statusCode: statusCode,
             httpVersion: nil,
             headerFields: nil
         )
-
-        var apiError: ApiError!
-
         let testTransport = TestTransport(responseData: Data(), urlResponse: urlResponse!)
         do {
-            _ = try await testTransport.send(request: request)
+            return try await testTransport.send(request: request)
         } catch {
-            apiError = error as? ApiError
+            throw error
         }
-        return apiError
+    }
+}
+
+// Extension found at:
+// https://www.wwt.com/article/unit-testing-on-ios-with-async-await
+extension XCTest {
+    func XCTAssertThrowsError<T: Sendable>(
+        _ expression: @autoclosure () async throws -> T,
+        _ message: @autoclosure () -> String = "",
+        file: StaticString = #filePath,
+        line: UInt = #line,
+        _ errorHandler: (_ error: Error) -> Void = { _ in }
+    ) async {
+        do {
+            _ = try await expression()
+            XCTFail(message(), file: file, line: line)
+        } catch {
+            errorHandler(error)
+        }
     }
 }
