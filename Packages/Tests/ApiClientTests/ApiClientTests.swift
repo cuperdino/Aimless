00f148a4de6f64f62b93d7ba1f4be066592ec4c7
Sabahudin Kodro <sabahudin.kodro@gmail.com>
2022-06-02 19:15:47 +0200

    Add tests to validate error handling

--- Packages/Tests/ApiClientTests/ApiClientTests.swift
+++ Packages/Tests/ApiClientTests/ApiClientTests.swift
@@ -5,17 +5,42 @@ final class ApiClientTests: XCTestCase {
 
     let apiClient = ApiClient()
 
-    func testFetchTodos() async throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-
-        let posts = try await apiClient.fetchTodos()
-        print(posts)
+    func testNotFoundError() async throws {
+        let apiError = await getApiError(statusCode: 404)
+        XCTAssertEqual(ApiError.notFound, apiError)
     }
 
-    func testFetchUsers() async throws {
-        let users = try await apiClient.fetchUsers()
-        print(users)
+    func testNotInternalServerError() async throws {
+        let apiError = await getApiError(statusCode: 500)
+        XCTAssertEqual(ApiError.interalServerError, apiError)
+    }
+
+    func testOtherError() async throws {
+        let apiError = await getApiError(statusCode: 504)
+        switch apiError {
+        case .other: assert(true)
+        default: assert(false)
+        }
+    }
+
+    private func getApiError(statusCode: Int) async -> ApiError {
+        let request = URLRequest(url: URL(string: "testurl.com")!)
+
+        let urlResponse = HTTPURLResponse(
+            url: request.url!,
+            statusCode: statusCode,
+            httpVersion: nil,
+            headerFields: nil
+        )
+
+        var apiError: ApiError!
+
+        let testTransport = TestTransport(responseData: Data(), urlResponse: urlResponse!)
+        do {
+            _ = try await testTransport.send(request: request)
+        } catch {
+            apiError = error as? ApiError
+        }
+        return apiError
     }
 }
