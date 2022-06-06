c5205a7e8cae2e39b19ee714ea819c218c7b285e
Sabahudin Kodro <sabahudin.kodro@gmail.com>
2022-06-06 20:09:08 +0200

    Add test for deletion synchronization

--- Packages/Tests/SynchronizationServiceTests/SynchronizationServiceTests.swift
+++ Packages/Tests/SynchronizationServiceTests/SynchronizationServiceTests.swift
@@ -36,7 +36,7 @@ class SynchronizationServiceTests: XCTestCase {
             let allTodos = TodoEntity.fetchRequest()
             let allCount = try context.count(for: allTodos)
 
-            XCTAssertEqual(10, allCount)
+            XCTAssertEqual(15, allCount)
             XCTAssertEqual(4, unsyncedTodos.count)
         }
     }
@@ -61,7 +61,7 @@ class SynchronizationServiceTests: XCTestCase {
                 }
             }
 
-            XCTAssertEqual(allCount, 10)
+            XCTAssertEqual(allCount, 15)
             XCTAssertEqual(unsyncedCount, 0)
             XCTAssertEqual(syncPendingCount, 4)
         }
@@ -106,7 +106,7 @@ class SynchronizationServiceTests: XCTestCase {
 
             XCTAssertEqual(notSyncedCount, 4)
             XCTAssertEqual(syncPendingCount, 0)
-            XCTAssertEqual(syncedCount, 6)
+            XCTAssertEqual(syncedCount, 11)
         }
 
         // Perform synchronization
@@ -134,7 +134,7 @@ class SynchronizationServiceTests: XCTestCase {
 
             XCTAssertEqual(notSyncedCount, 0)
             XCTAssertEqual(syncPendingCount, 0)
-            XCTAssertEqual(syncedCount, 10)
+            XCTAssertEqual(syncedCount, 15)
         }
     }
 
@@ -165,7 +165,7 @@ class SynchronizationServiceTests: XCTestCase {
             let unsyncedTodosCount = try context.count(for: unsyncedTodosRequest)
             let allTodosCount = try context.count(for: allTodosRequest)
 
-            XCTAssertEqual(allTodosCount, 10)
+            XCTAssertEqual(allTodosCount, 15)
             XCTAssertEqual(unsyncedTodosCount, 4)
         }
 
@@ -176,7 +176,7 @@ class SynchronizationServiceTests: XCTestCase {
             let unsyncedTodosCount = try context.count(for: unsyncedTodosRequest)
             let allTodosCount = try context.count(for: allTodosRequest)
 
-            XCTAssertEqual(allTodosCount, 10)
+            XCTAssertEqual(allTodosCount, 15)
             XCTAssertEqual(unsyncedTodosCount, 4)
         }
 
@@ -190,8 +190,81 @@ class SynchronizationServiceTests: XCTestCase {
             let unsyncedTodosCount = try context.count(for: unsyncedTodosRequest)
             let allTodosCount = try context.count(for: allTodosRequest)
 
-            XCTAssertEqual(allTodosCount, 10)
+            XCTAssertEqual(allTodosCount, 15)
             XCTAssertEqual(unsyncedTodosCount, 4)
+        }
+    }
+
+    func testDeletionSynchronization() async throws {
+        // Setup
+        let context = self.persistenceService.backgroundContext
+        let testTransport = TestTransport(
+            responseData: createPostResponse(),
+            urlResponse: .success
+        )
+        let apiClient = ApiClient(transport: testTransport)
+        let dataImporter = DataImporterService(apiClient: apiClient, persistenceService: persistenceService)
+        let synchronizationService = SynchronizationService(
+            apiClient: apiClient,
+            persistenceService: persistenceService,
+            dataImporter: dataImporter
+        )
+
+        // Validate state before synchronization
+        try await context.perform {
+            let allTodos = try context.fetch(TodoEntity.fetchRequest())
+
+            var notDeleted = 0
+            var deletionPending = 0
+            var deleted = 0
+
+            for todo in allTodos {
+                if todo.deletionState == .notDeleted {
+                    notDeleted += 1
+                }
+
+                if todo.deletionState == .deletionPending {
+                    deletionPending += 1
+                }
+
+                if todo.deletionState == .deleted {
+                    deleted += 1
+                }
+            }
+
+            XCTAssertEqual(notDeleted, 10)
+            XCTAssertEqual(deletionPending, 0)
+            XCTAssertEqual(deleted, 5)
+        }
+
+        // Perform synchronization
+        await synchronizationService._deletionSynchronization(context: context)
+
+        // Validate state after synchronization
+        try await context.perform {
+            let allTodos = try context.fetch(TodoEntity.fetchRequest())
+
+            var notDeleted = 0
+            var deletionPending = 0
+            var deleted = 0
+
+            for todo in allTodos {
+                if todo.deletionState == .notDeleted {
+                    notDeleted += 1
+                }
+
+                if todo.deletionState == .deletionPending {
+                    deletionPending += 1
+                }
+
+                if todo.deletionState == .deleted {
+                    deleted += 1
+                }
+            }
+
+            XCTAssertEqual(notDeleted, 10)
+            XCTAssertEqual(deletionPending, 0)
+            XCTAssertEqual(deleted, 0)
         }
     }
 
@@ -216,6 +289,16 @@ class SynchronizationServiceTests: XCTestCase {
                 todoEntity.title = "Some title"
                 todoEntity.updatedAt = Date.now
                 todoEntity.synchronizationState = .synchronized
+                todoEntity.completed = false
+                todoEntity.userId = number
+            }
+            for number in 11...15 {
+                let todoEntity = TodoEntity(context: context)
+                todoEntity.id = number
+                todoEntity.title = "Some title"
+                todoEntity.updatedAt = Date.now
+                todoEntity.synchronizationState = .synchronized
+                todoEntity.deletionState = .deleted
                 todoEntity.completed = false
                 todoEntity.userId = number
             }
