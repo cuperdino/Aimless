5d5560d002ce2f5c1382dc5fae2e10866a123463
Sabahudin Kodro <sabahudin.kodro@gmail.com>
2022-06-06 10:44:49 +0200

    Revert "Remove user entity"
    This reverts commit 2f9affda62444af42574de9b10a7f35cc592a597.

---
+++ Packages/Sources/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  
+//
+//  Created by Sabahudin Kodro on 03/06/2022.
+//
+
+import Foundation
+
+public struct User: Codable {
+    public let id: Int
+    public let name: String
+    public let username: String
+    public let email: String
+}
